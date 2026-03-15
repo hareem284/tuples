f52d5d4718433074c2fe6632bb085ce00be1bb4a
hareem284 <sehrishandaleeb06@gmail.com>
2026-03-15 13:33:06 +0500

    commit

---
+++ set.py
@@ -0,0 +1,10 @@
+a={1,2,4,5,6}
+a.add(3)
+print(a)
+b={3,5,9,12}
+e=a.intersection(b)
+print("Intersection is ",e)
+u=a.union(b)
+print("Union is " ,u)
+d=a.difference(b)
+print("Difference is. ",d)
