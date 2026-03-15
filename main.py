a4f1a7f97d48de7008f68ec8ffa056e1b90389d5
hareem284 <sehrishandaleeb06@gmail.com>
2026-03-15 13:23:16 +0500

    commit

---
+++ main.py
@@ -0,0 +1,8 @@
+my_tupil=()
+int_tupil=(1,4,2,6)
+mix_tupil=(9,'lauren',2.4)
+nested_tupil=(('hareem','emaan','izzah'),[4,7,6,8,3],True)
+print(nested_tupil[1])
+print(nested_tupil[1][3])
+for t in mix_tupil:
+   print(t)
